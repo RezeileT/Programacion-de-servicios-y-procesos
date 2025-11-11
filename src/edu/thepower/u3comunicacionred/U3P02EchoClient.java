9b05af000cce0d8938bc2e73c189fe46cf620fa2
AlumnoAfternoon <sebastianbarrios1812@gmail.com>
2025-11-11 21:14:57 +0100

    11/11/25 - Nuevo temario conección de red

--- src/edu/thepower/u3comunicacionred/U3P02EchoClient.java
+++ src/edu/thepower/u3comunicacionred/U3P02EchoClient.java
@@ -1,19 +1,26 @@
 package edu.thepower.u3comunicacionred;
 import java.io.*;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class U3P02EchoClient {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         try (Socket socket = new Socket("localhost", 1025)) {
             OutputStream os = socket.getOutputStream();
             PrintWriter pw = new PrintWriter(os, true);
             InputStream is = socket.getInputStream();
             BufferedReader br = new BufferedReader(new InputStreamReader(is));
-            pw.println("ECHO SERVIDOR");
-            String line = null;
-            while ((line = br.readLine()) != null) {
-                System.out.println("Recibido del servidor: " + line);
-            }
+            String entrada;
+            do{
+                entrada = sc.nextLine();
+                pw.println(entrada);
+                String line = null;
+                System.out.println("Recibido del servidor: " + br.readLine());
+
+            }while (!entrada.equals("0"));
+
+
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
